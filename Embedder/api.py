eabda80bade1e2846a5792daf29b67a677d5eda7
Bohdan Boklah <b.boklag@gmail.com>
2025-09-01 18:36:07 +0300

    init API for Embedder service

---
+++ Embedder/api.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+import uvicorn
+from Embedder.config import MODEL_PATH, PORT, HOST, DEVICE
+from Embedder.dto.embed_request import EmbedRequest
+from Embedder.dto.embed_response import EmbedResponse
+
+model = SentenceTransformer(MODEL_PATH, device=DEVICE)
+
+app = FastAPI(title="Embedder Service")
+
+@app.post("/embed", response_model=EmbedResponse)
+async def embed(req: EmbedRequest):
+    vectors = model.encode(req.texts, normalize_embeddings=True).tolist()
+    return {"embeddings": vectors}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=HOST, port=PORT)
